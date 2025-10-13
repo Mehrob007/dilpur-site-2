f239292aa2922c75c6a2d2d63e675c5bc826487e
Mehrob007 <mehrobsanginov2006@gmail.com>
2025-10-13 15:55:49 +0500

    create: new type phone for input

--- src/components/element/Input.tsx
+++ src/components/element/Input.tsx
@@ -1,5 +1,6 @@
 import { InputT } from "@/types/elements";
-import React from "react";
+import React, { ChangeEvent } from "react";
+import ReactInputMask from "@mona-health/react-input-mask";
 
 export default function Input({
   id,
@@ -8,21 +9,45 @@ export default function Input({
   placeholder,
   title,
   errors,
+  type = "text",
 }: InputT) {
+  const handleSubmit = (event: ChangeEvent<HTMLInputElement>) => {
+    event.preventDefault();
+    const cleanPhone = event.target.value.replace(/\D/g, "");
+    onChange(cleanPhone);
+  };
+
   return (
-    <label htmlFor={id} className="input">
+    <label
+      htmlFor={id}
+      className={`input ${type === "phone" ? "input-phone" : ""}`}
+    >
       {title && (
         <span className={!value?.length ? "input-placeholder" : ""}>
           {title}
         </span>
       )}
-      <input
-        id={id}
-        type="text"
-        value={value ?? ""}
-        placeholder={placeholder}
-        onChange={(e) => onChange(e.target.value)}
-      />
+      {type === "phone" ? (
+        <ReactInputMask
+          mask="(99) 999-99-99"
+          maskChar="___"
+          value={value}
+          onChange={handleSubmit}
+          type="tel"
+          id="phone-input"
+          className="form-control"
+          placeholder="(XX) XXX-XX-XX"
+          required
+        />
+      ) : (
+        <input
+          id={id}
+          type={type}
+          value={value ?? ""}
+          placeholder={placeholder}
+          onChange={(e) => onChange(e.target.value)}
+        />
+      )}
       <p style={{ opacity: errors?.[id] ? 1 : 0 }}>{errors?.[id]}</p>
     </label>
   );
