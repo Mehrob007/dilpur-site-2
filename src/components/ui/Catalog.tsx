325bfa0a39f8e185944da992a2804c1090a61192
Mehrob007 <mehrobsanginov2006@gmail.com>
2025-12-27 23:48:37 +0500

    feat: implement product catalog with search, filtering, pagination, and dedicated types

--- src/components/ui/Catalog.tsx
+++ src/components/ui/Catalog.tsx
@@ -1,12 +1,21 @@
+"use client";
 import React from "react";
 import ProductItems from "@/modules/product/ProductItems";
 import GiftСard from "@/components/ui/GiftСard";
 import PartnerStores from "@/components/ui/PartnerStores";
+import { useSearchParams } from "next/navigation";
 
 export default function Catalog() {
+  const searchParams = useSearchParams();
+  const name = searchParams.get("name");
+
   return (
     <div className="catalog">
-      <ProductItems title={"Каталог"} type={"filter"} />
+      <ProductItems
+        title={name?.length ? "Найдено" : "Каталог"}
+        type={"filter"}
+        showCount={Boolean(name?.length)}
+      />
       <GiftСard />
       <PartnerStores />
     </div>
