af023fee664dae5d83b392e52c88fdb2c9b4a4dc
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-01-07 06:22:52 +0500

    feat: Add global styling, Header component, and Favorites page.

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import PartnerStores from "@/components/ui/PartnerStores";
+import ProductItems from "@/modules/product/ProductItems";
+import React from "react";
 
 export default function page() {
   return (
-    <div>page</div>
-  )
+    <div className="catalog">
+      <ProductItems title={"Избранное"} type={null} />
+      <PartnerStores />
+    </div>
+  );
 }
