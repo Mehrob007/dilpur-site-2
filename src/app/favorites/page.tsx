3bc7dbda7be84d75b240a2cc580fc059c7d110fe
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-01-13 18:59:23 +0500

    refactor: simplify favorites page structure

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,12 +1,7 @@
-import PartnerStores from "@/components/ui/PartnerStores";
-import ProductItems from "@/modules/product/ProductItems";
-import React from "react";
+import React from 'react'
 
 export default function page() {
   return (
-    <div className="catalog">
-      <ProductItems title={"Избранное"} type={null} />
-      <PartnerStores />
-    </div>
-  );
+    <div>page</div>
+  )
 }
