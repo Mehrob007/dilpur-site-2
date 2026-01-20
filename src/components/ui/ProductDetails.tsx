4254f50464cdafb2edea56e3b37fb8bf444e8610
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-01-20 16:42:53 +0500

    refactor: Update product details component to accept a tag array and a calculated pre-cost percentage for display.

--- src/components/ui/ProductDetails.tsx
+++ src/components/ui/ProductDetails.tsx
@@ -1,13 +1,15 @@
 import { detailsT } from "@/types/product";
 import React from "react";
 
-export default function ProductDetails({ details }: detailsT) {
+export default function ProductDetails({ details, preCostProcent }: detailsT) {
   return (
     <div className="product-item-details">
-      {details && typeof details === "object" && (
-        <span className="product-item-img-discount">-{details.discount}%</span>
+      {preCostProcent ? (
+        <span className="product-item-img-discount">{preCostProcent}%</span>
+      ) : (
+        ""
       )}
-      {details && typeof details === "object" ? (
+      {details?.find((e) => e === "Новинка") ? (
         <span className="product-item-img-new">НОВИНКА</span>
       ) : (
         ""
