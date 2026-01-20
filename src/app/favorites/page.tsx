94da8ccbe817e34210b298185b362cf5d0ed667c
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-01-20 15:41:14 +0500

    feat: Implement favorites page with dedicated components and update product type definitions for image handling.

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+"use client";
+import GiftСard from "@/components/ui/GiftСard";
+import PartnerStores from "@/components/ui/PartnerStores";
+import FavoritesItems from "@/modules/favorites/FavoritesItems";
+import { useStore } from "@/store/globalState";
+import React from "react";
 
-export default function page() {
+export default function Page() {
+  const { propertys } = useStore();
+
+  console.log("propertys", propertys);
+
   return (
-    <div>page</div>
-  )
+    <div className="catalog">
+      <FavoritesItems title="Избранное" ids={propertys} />
+      <GiftСard />
+      <PartnerStores />
+    </div>
+  );
 }
