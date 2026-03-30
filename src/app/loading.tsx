4971feb67adf2d7584955f8a2d20a3a0d4c927d9
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-03-30 22:19:16 +0500

    refactor: update UI transitions, add loading state, and refine basket component layout

---
+++ src/app/loading.tsx
@@ -0,0 +1,35 @@
+"use client";
+import React from "react";
+
+export default function Loading() {
+  return (
+    <div className="loading-container">
+      <div className="loader"></div>
+      <style jsx>{`
+        .loading-container {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          height: 50vh;
+          width: 100%;
+        }
+        .loader {
+          width: 40px;
+          height: 40px;
+          border: 3px solid #f3f3f3;
+          border-top: 3px solid #181818;
+          border-radius: 50%;
+          animation: spin 1s linear infinite;
+        }
+        @keyframes spin {
+          0% {
+            transform: rotate(0deg);
+          }
+          100% {
+            transform: rotate(360deg);
+          }
+        }
+      `}</style>
+    </div>
+  );
+}
