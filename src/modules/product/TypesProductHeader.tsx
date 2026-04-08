d2543ec612c1fa8f67b00d8a137984958d26531d
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-04-08 22:49:36 +0500

    feat: add product header component and update global styles

--- src/modules/product/TypesProductHeader.tsx
+++ src/modules/product/TypesProductHeader.tsx
@@ -4,8 +4,8 @@ import { optionSort } from "@/constants/select";
 import { useGlobalState } from "@/store/globalState";
 import { sizeT, TypesProductHeaderT } from "@/types/product";
 import Image from "next/image";
-import { usePathname, useRouter } from "next/navigation";
-import React, { useEffect, useState } from "react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import React, { useEffect, useState, useCallback } from "react";
 import arrow1Icon from "@/../public/icons/arrow-1.svg";
 import { GetSizeREQ } from "@/api/product/size";
 import { GetTypeREQ } from "@/api/type/type";
@@ -13,46 +13,58 @@ import { GetCategoryREQ } from "@/api/product/category";
 
 export default function TypesProductHeader({ type }: TypesProductHeaderT) {
   const pathName = usePathname();
+  const searchParams = useSearchParams();
   const { setClearSearch } = useGlobalState();
   const router = useRouter();
   const [size, setSize] = useState<sizeT[] | []>([]);
   const [types, setTypes] = useState<sizeT[] | []>([]);
   const [category, setCategory] = useState<sizeT[] | []>([]);
 
-  const getDataSize = async () => {
+  const selectedTypes = searchParams.getAll("types");
+  const activeTypeId =
+    selectedTypes.length > 0
+      ? Number(selectedTypes[selectedTypes.length - 1])
+      : undefined;
+
+  const getDataSize = useCallback(async (typeId?: number) => {
     try {
-      const res = await GetSizeREQ({});
+      const res = await GetSizeREQ({ TypeId: typeId });
       setSize(res.data);
     } catch (e) {
       console.log(e);
       setSize([]);
     }
-  };
-  const getDataType = async () => {
+  }, []);
+
+  const getDataType = useCallback(async () => {
     try {
-      const res = await GetTypeREQ({});
+      const sender = pathName?.includes("/female") ? 1 : 0;
+      const res = await GetTypeREQ({ Gender: sender });
       setTypes(res.data);
     } catch (e) {
       console.log(e);
       setTypes([]);
     }
-  };
+  }, [pathName]);
 
-  const getDataCategory = async () => {
+  const getDataCategory = useCallback(async (typeId?: number) => {
     try {
-      const res = await GetCategoryREQ({});
+      const res = await GetCategoryREQ({ TypeId: typeId });
       setCategory(res.data);
     } catch (e) {
       console.log(e);
       setCategory([]);
     }
-  };
+  }, []);
 
   useEffect(() => {
-    getDataSize();
     getDataType();
-    getDataCategory();
-  }, []);
+  }, [getDataType]);
+
+  useEffect(() => {
+    getDataSize(activeTypeId);
+    getDataCategory(activeTypeId);
+  }, [activeTypeId, getDataSize, getDataCategory]);
 
   if (type === "filter") {
     return (
