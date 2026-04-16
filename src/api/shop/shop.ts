3b8d56710db995683743edf51dc8757ecdc1df3c
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-04-16 11:04:22 +0500

    feat: implement shop modal retrieval and update product navigation logic

--- src/api/shop/shop.ts
+++ src/api/shop/shop.ts
@@ -1,8 +1,18 @@
+const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 import apiClient from "@/utils/apiClient";
 
 export const GetShopREQ = async () => {
   try {
     const res = await apiClient(`shops`);
+    return res.data;
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+export const GetShopByIdREQ = async (shopId: number) => {
+  try {
+    const res = await apiClient(`${BASE_URL}` + shopId);
     return res.data;
   } catch (e) {
     console.error(e);
