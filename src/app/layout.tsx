a4db75a979c5e6a725b6ed26608273df31b5dc18
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-04-18 19:16:21 +0500

    fix: add null checks for API responses, update viewport meta tags, and enhance mobile navigation UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import "@/styles/globalStyles.css";
+import "@/styles/globalStyles.scss";
 import Header from "@/components/ui/Header";
 import Footer from "@/components/ui/Footer";
 import PageTransition from "@/components/ui/PageTransition";
@@ -21,6 +21,11 @@ export const metadata: Metadata = {
   description: " ",
 };
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+};
+
 import NextTopLoader from "nextjs-toploader";
 
 export default function RootLayout({
@@ -30,6 +35,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
+      </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <NextTopLoader color="#181818" showSpinner={false} />
         <Header />
