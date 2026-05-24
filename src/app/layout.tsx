5c89a0f2419bd8e831954822e6396d3c13479c64
Mehrob007 <mehrobsanginov2006@gmail.com>
2026-05-24 22:52:20 +0500

    refactor: remove unused Geist font imports from root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import "@/styles/globalStyles.scss";
 import Header from "@/components/ui/Header";
